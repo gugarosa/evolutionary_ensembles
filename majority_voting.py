50df8ee51967df2af87f50f320a52e34de4880a9
Gustavo Rosa <gth.rosa@uol.com.br>
2020-01-28 13:58:54 -0300

    Adding time calculation to majority voting script.

--- majority_voting.py
+++ majority_voting.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import models.ensemble as e
 import utils.constants as c
@@ -34,6 +35,7 @@ if __name__ == '__main__':
 
     # Creating empty lists to save outputs
     val_accs, test_accs = [], []
+    val_time, test_time = [], []
 
     print(f'\nPerforming majority voting over {dataset} ...\n')
 
@@ -46,12 +48,24 @@ if __name__ == '__main__':
 
         # Loads the testing step predictions and labels
         test_pred, test_y = l.load_candidates(dataset, 'test', k+1)
+        
+        # Defining the starting time of validation majority voting
+        start_v = time.time()
 
         # Gather the majority votes between validation predictions
         val_votes = e.majority_voting(val_pred)
 
+        # Defining the ending time of validation majority voting
+        end_v = time.time()
+
+        # Defining the starting time of testing majority voting
+        start_t = time.time()
+
         # Gather the majority votes between test predictions
         test_votes = e.majority_voting(test_pred)
+
+        # Defining the ending time of testing majority voting
+        end_t = time.time()
 
         # Calculates the validation accuracy
         val_acc = m.accuracy(val_votes, val_y)
@@ -64,6 +78,8 @@ if __name__ == '__main__':
         # Appending values to lists
         val_accs.append(val_acc)
         test_accs.append(test_acc)
+        val_time.append(end_v - start_v)
+        test_time.append(end_t - start_t)
 
     print('Saving outputs ...')
 
@@ -71,8 +87,16 @@ if __name__ == '__main__':
     with open(f'output/mv_{dataset}_val.txt', 'w') as f:
         f.writelines([f'{acc}\n' for acc in val_accs])
 
+    # Saving validation times to file
+    with open(f'output/mv_{dataset}_val.time', 'w') as f:
+        f.writelines([f'{time}\n' for time in val_time])
+
     # Saving testing accuracies to file
     with open(f'output/mv_{dataset}_test.txt', 'w') as f:
         f.writelines([f'{acc}\n' for acc in test_accs])
+
+    # Saving testing times to file
+    with open(f'output/mv_{dataset}_test.time', 'w') as f:
+        f.writelines([f'{time}\n' for time in test_time])
 
     print('Outputs saved.')
